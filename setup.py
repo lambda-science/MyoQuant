e844fb1d5e49108ee183fdfa018cc0b0030e2d53
Corentin <>
2023-01-17 17:02:07 +0100

    remove unused setup

--- setup.py
+++ setup.py
@@ -5,9 +5,4 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],
-    # entry_points={
-    #     "console_scripts": [
-    #         "myoquant = myoquant.__main__:app",
-    #     ],
-    # },
 )
